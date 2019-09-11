aac37a3970530c8b6c2af272045708bb2972c624
Hyunsoo Woo <53418319+whs9210@users.noreply.github.com>
2019-09-11 17:49:40 +0900

    Add files via upload

---
+++ ProductDTO.java
@@ -0,0 +1,41 @@
+package db연결연습;
+
+public class ProductDTO {
+	String id;
+	String name;
+	String content;
+	String price;
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setContent(String content) {
+		this.content = content;
+	}
+
+	public String getContent() {
+		return content;
+	}
+
+	public void setPrice(String price) {
+		this.price = price;
+	}
+
+	public String getPrice() {
+		return price;
+	}
+
+}
